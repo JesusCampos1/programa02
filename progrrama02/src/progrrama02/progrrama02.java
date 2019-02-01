5ffa085ed38d7e97dded7a3051355f7ecc8d81ea
jsusc <jsusc@DESKTOP-SKGNO7S>
2019-01-31 19:44:08 -0600

    programa02

---
+++ progrrama02/src/progrrama02/progrrama02.java
@@ -0,0 +1,17 @@
+package progrrama02;
+
+import java.util.Scanner;
+
+public class progrrama02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leer = new Scanner(System.in);
+		System.out.print("Ingrese su nombre ");
+		String nombre = leer.nextLine();
+		
+		System.out.println("Hola "+nombre);
+
+	}
+
+}
